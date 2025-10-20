ca70283d0c2dfb1ea104a406e95371d27e14f140
hagjibe01 <Hagji.Beciri@tecalliance.net>
2025-10-20 22:04:48 +0200

    refactor saving location from PropertyFileUserManagerImpl.java to PostgresUserManagerImpl and add the connection between with the repository UserRepository.java

--- src/main/java/com/carpool/demo/model/user/User.java
+++ src/main/java/com/carpool/demo/model/user/User.java
@@ -20,6 +20,9 @@ public class User {
 
  public User(){}
 
+    public int  getUserid() {
+        return userid;
+    }
     public String getName() {return name;}
     public String getMobileNumber() {return mobileNumber;}
     // public String getEmail() {return email;}
@@ -27,6 +30,8 @@ public class User {
     public String getToken() {return token;}
     public long getTokenExpiration() {return tokenExpiration;}
 
+
+    public void setUserid(int userid) {}
     public void setName(String name) {this.name = name;}
     public void setMobileNumber(String mobileNumber) {this.mobileNumber = mobileNumber;}
     // public void setEmail(String email) {this.email = email;}
